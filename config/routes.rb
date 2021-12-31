2beb641ecfd5b1ff4da032184b332c1a5e498c56
Henrique Barros <henriqueeustaquiob@gmail.com>
2021-12-30 22:14:15 -0300

    CORS
    Commit do cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "password_panel_dbs#index"
 
-  resources :password_panel_dbs
+  resources :password_panel_dbs, :default => {format: :json}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'chamar', to: 'password_panel_dbs#chamar'
   get 'cancel/:id', to: 'password_panel_dbs#cancel'
