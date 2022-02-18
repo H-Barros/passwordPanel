b8e0af2f968c0fc6cf0c3001cd2dc2ee90ed6827
Henrique Barros <henriqueeustaquiob@gmail.com>
2022-02-17 23:54:28 -0300

    Update password_channel.rb

--- app/channels/password_channel.rb
+++ app/channels/password_channel.rb
@@ -1,10 +1,5 @@
 class PasswordChannel < ApplicationCable::Channel
   def subscribed
-    self.number_off_tickets
     stream_from "password_channel"
-  end
-
-  def number_off_tickets
-    PasswordPanelDb.have_password_in_queue?
   end
 end
